4dc1401053c2c8d0db529355168f86b1d4c47cb0
Mary Kroustali <71144000+MaryKroustali@users.noreply.github.com>
2024-10-23 13:55:45 +0300

    Create Query1proced.sql

---
+++ Procedures/Query1proced.sql
@@ -0,0 +1,24 @@
+﻿USE [Chinook]
+GO
+/****** Object:  StoredProcedure [dbo].[query1]    Script Date: 16/1/2021 10:41:06 πμ ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[query1] 
+ @X INT, --κορυφαίοι X καλλιτέχνες
+ @StartDate DATETIME,  --ημερομηνία αναζήτησης
+ @StopDate DATETIME
+AS
+ BEGIN
+    SELECT TOP (@X) Artist.Name, Artist.ArtistId, COUNT (Album.Title) as 'Album Sales' --πλήθος άλμπουμ που πωλήθηκαν
+FROM Artist, InvoiceLine, Album, Invoice, Track 
+WHERE Invoice.InvoiceDate > @StartDate AND --χρονικό διάστημα με βάση τα τιμολόγια
+	  Invoice.InvoiceDate < @StopDate AND 
+      Invoice.InvoiceId = InvoiceLine.InvoiceId AND
+	  InvoiceLine.TrackId = Track.TrackId AND  
+	  Track.AlbumId = Album.AlbumId AND
+	  Album.ArtistId = Artist.ArtistId
+	  GROUP BY Artist.Name, Artist.ArtistId         --άλμπουμ ανά καλλιτέχνη
+	  ORDER BY [Album Sales] DESC  --ταξινόμηση πωλήσεων με φθίνουσα σειρά
+END
